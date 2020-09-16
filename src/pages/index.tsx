e6218281c55167b5d46a816dc05a27ec67c93a3c
constantinos <konospas@gmail.com>
2020-09-16 16:40:45 +0300

    added material ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import Layout from '../layout/layout'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 
 class IndexPage extends React.Component {
 
@@ -9,6 +11,13 @@ class IndexPage extends React.Component {
       <Layout>
         <div>
           Main content
+          <div className={`buttons`}>
+            <ButtonGroup>
+              <Button variant={`contained`} color={`primary`}>Button1</Button>
+              <Button variant={`text`} color={`secondary`}>Button2</Button>
+              <Button variant={`outlined`} color={`default`}>Button3</Button>
+            </ButtonGroup>
+          </div>
         </div>
       </Layout>
     )
